2af17bfba3684600838303d9e7385de755c55569
thefirsthouse <vasilijanenko4@gmail.com>
2025-02-27 21:36:25 +0300

    created app, realized login and logout

---
+++ finance_tracker/accounts/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+# class RegisterForm(forms):
+#     username = forms.CharField(max_length=20, min_length=3, required=True, 
+#         widget=forms.TextInput({'class': 'form-input'}), label='Username')
+#     email = forms.EmailField(label='Email')
+#     password = forms.PasswordInput(attrs={'class': 'form-input'})
---
+++ finance_tracker/accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
+# from .forms import LoginUserForm
+
+
+def login_user(request):
+    if request.method == "POST":
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], email=
+                cd['email'], password=cd['password'])
+            if user and user.is_active:
+                login(request, user)
+            return HttpResponseRedirect(reverse('home'))
+    return render(request, 'accounts/login', context={'form': form})
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
