5f4fae3db75c499e03ad55e25faf38d010761ad2
thefirsthouse <vasilijanenko4@gmail.com>
2025-03-30 15:07:20 +0300

    fixed logout button

--- finance_tracker/accounts/urls.py
+++ finance_tracker/accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path("logout/", LogoutView.as_view(), name='logout'),
+    path("logout/", views.logout_user, name='logout'),
     path("register/", views.register, name='register'),
     path("profile/", views.profile, name='profile'),
     path("edit_profile/", views.edit_profile, name='edit_profile'),
