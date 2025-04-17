f41d1d6e24c84a5565c17661805808cc618afb9d
thefirsthouse <vasilijanenko4@gmail.com>
2025-04-17 18:17:28 +0300

    added record list

--- finance_tracker/interface/urls.py
+++ finance_tracker/interface/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 import interface.views as views  # Можно оставить так или заменить на from . import views
+import logic.views as logic_views
 
 urlpatterns = [
     path("", views.index, name='main'),
+    path("records/", views.records_list, name='records'),
+    path("records/create", logic_views.create_transfer, name='create_transfer'),
 ]
--- finance_tracker/interface/views.py
+++ finance_tracker/interface/views.py
@@ -8,3 +8,18 @@ def index(request):
     accounts = Account.objects.filter(user=request.user)
     records = Record.objects.filter(account__in=accounts).order_by('-date_time')[:5]
     return render(request, "interface/index.html", {"accounts": accounts, "records": records})
+
+
+def records_list(request):
+    sort = request.GET.get('sort')
+
+    if sort == 'date':
+        records = Record.objects.order_by('-date_time')
+    elif sort == 'category':
+        records = Record.objects.order_by('category__name')
+    elif sort == 'type':
+        records = Record.objects.order_by('type_of')
+    else:
+        records = Record.objects.all().order_by('-date_time')
+    
+    return render(request, 'interface/record_list.html', {'records': records})
